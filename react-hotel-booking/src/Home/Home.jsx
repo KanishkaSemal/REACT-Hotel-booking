98dd37b797246a7021f4baac61adc95875a7144b
Kanishka Semal <155632293+KanishkaSemal@users.noreply.github.com>
2024-09-23 17:19:29 +0530

    finished

--- react-hotel-booking/src/Home/Home.jsx
+++ react-hotel-booking/src/Home/Home.jsx
@@ -106,10 +106,162 @@ function Home() {
               </select>
             </div>
           </div>
+          <input type="submit" value="check availability" name="check" className="btn"></input>
         </form>
       </section>
+
+
+
+
+      <section className="about" id="about">
+      <div className="row">
+        <div className="image">
+          <img src="images/about-img-1.jpg" alt="" />
+        </div>
+        <div className="content">
+          <h3>Best Staff</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi laborum maxime alias aliquid temporibus unde?</p>
+          <a href="#reservation" className="btn1">
+            Make a reservation
+          </a>
+        </div>
+      </div>
+
+      <div className="row revers">
+        <div className="image">
+          <img src="images/about-img-2.jpg" alt="" />
+        </div>
+        <div className="content">
+          <h3>Best foods</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi laborum maxime alias aliquid temporibus unde?</p>
+          <a href="#contact" className="btn1">
+            contact us
+          </a>
+        </div>
+      </div>
+
+      <div className="row ">
+        <div className="image">
+          <img src="images/about-img-3.jpg" alt="" />
+        </div>
+        <div className="content">
+          <h3>swimming pool</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi laborum maxime alias aliquid temporibus unde?</p>
+          <a href="#availability" className="btn1">
+            check availability
+          </a>
+        </div>
+      </div>
+    </section>
+
+
+
+
+    <section className="services">
+      <div className="box-container">
+        <div className="box">
+          <img src="images/icon-1.png" alt="" />
+          <h3>Food & Drinks</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, sunt?</p>
+        </div>
+
+        <div className="box">
+          <img src="images/icon-2.png" alt="" />
+          <h3>Outdoor Dining</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, sunt?</p>
+        </div>
+
+        <div className="box">
+          <img src="images/icon-3.png" alt="" />
+          <h3>Beach View</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, sunt?</p>
+        </div>
+
+        <div className="box">
+          <img src="images/icon-4.png" alt="" />
+          <h3>Decorations</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, sunt?</p>
+        </div>
+
+        <div className="box">
+          <img src="images/icon-5.png" alt="" />
+          <h3>Swimming Pool</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, sunt?</p>
+        </div>
+
+        <div className="box">
+          <img src="images/icon-6.png" alt="" />
+          <h3>Resort Beach</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, sunt?</p>
+        </div>
+      </div>
+    </section>
+   
+    
+ 
+ <section class="reservation" id="reservation">
+  
+    <form action="" method="post">
+ <h3>make a reservation</h3>
+ <div class="flex">
+ <div class="box">
+ <p>check in <span>*</span></p>
+ <input type="date" name="check_in" class="input" required></input>
+ </div>
+ <div class="box">
+ <p>check out <span>*</span></p>
+ <input type="date" name="check_out" class="input" required></input>
+ </div>
+ <div class="box">
+ <p>adults <span>*</span></p>
+ <select name="adults" class="input" required>
+ <option value="1">1 adult</option>
+ <option value="2">2 adults</option>
+ <option value="3">3 adults</option>
+ <option value="4">4 adults</option>
+ <option value="5">5 adults</option>
+ <option value="6">6 adults</option>
+ </select>
+ </div>
+ <div class="box">
+ <p>childs <span>*</span></p>
+ <select name="childs" class="input" required>
+ <option value="-">0 child</option>
+ <option value="1">1 child</option>
+ <option value="2">2 childs</option>
+ <option value="3">3 childs</option>
+ <option value="4">4 childs</option>
+ <option value="5">5 childs</option>
+ <option value="6">6 childs</option>
+ </select>
+ </div>
+ <div class="box">
+ <p>rooms <span>*</span></p>
+ <select name="rooms" class="input" required>
+ <option value="1">1 room</option>
+ <option value="2">2 rooms</option>
+ <option value="3">3 rooms</option>
+ <option value="4">4 rooms</option>
+ <option value="5">5 rooms</option>
+ <option value="6">6 rooms</option>
+ </select>
+ </div>
+ </div>
+ <input type="submit" value="check availability" name="check" class="btn"></input>
+ </form>
+ </section>
+
+ 
+
+
+
+ 
+
+    
+    
     </div>
   );
 }
+
 
 export default Home;
